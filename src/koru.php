af25045bc0b2e5184baefd3c3cc89fe8579e801d
Yami Odymel <yamiodymel@gmail.com>
2016-04-03 13:10:26 +0000

    Added isEmpty(), isNull(), jsonDecode(), get(), set().

--- src/koru.php
+++ src/koru.php
@@ -20,22 +20,52 @@ class KoruData
 
     function __get($name)
     {
-        return isset($this->data[$name]) ? $this->data[$name] : null;
+        return $this->get($name);
     }
 
     function __set($name, $value)
     {
-        $this->data[$name] = $value;
+        return $this->set($name, $value);
+    }
+
+    function jsonDecode($name)
+    {
+        if(isset($this->data[$name]))
+            $this->set($name, json_decode($this->data[$name]));
+            
+        return $this;
     }
 
     function store($array)
     {
         foreach($array as $key => $value)
-        {
-            $this->data[$key] = $value;
-        }
+            $this->set($key, $value);
+        
+        return $this;
     }
-
+    
+    function get($name)
+    {
+        return isset($this->data[$name]) ? $this->data[$name] : null;
+    }
+    
+    function set($name, $value)
+    {
+        $this->data[$name] = $value;
+        
+        return $this;
+    }
+    
+    function isEmpty($name)
+    {
+        return empty($this->get($name));
+    }
+    
+    function isNull($name)
+    {
+        return is_null($this->get($name));
+    }
+    
     function isTroubled($set = false)
     {
         if($set)
